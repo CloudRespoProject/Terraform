59b10eacc9a969f80149e4879372c176e4510220
Payal Deval <144565485+CloudRespoProject@users.noreply.github.com>
2024-02-06 18:15:14 -0500

    Create jenkins CI/CD pipeline on AWS

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,5 @@
-#outputs.tf
-/*This code defines an output called alb_public_url with the description "Public URL for Application Load Balancer" 
-and the value of the DNS name of the Application Load Balancer. This output can be used to get the public URL of the 
-Application Load Balancer after Terraform has been applied. */
-# Terraform Outputs
-output "alb_public_url" {
-  description = "Public URL for Application Load Balancer"
-  value       = aws_lb.alb.dns_name
+output "public_url" {
+    description = "Public URL for Jenkins Server"
+    value = "http://${aws_instance.jenkins.public_ip}:8080"
+  
 }
-#
